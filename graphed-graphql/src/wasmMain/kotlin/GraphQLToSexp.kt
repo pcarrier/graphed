4de1077dc0a63afc51ceae520e7ff6d96b53bede
Pierre Carrier <pc@rrier.fr>
2023-05-29 22:18:59 +0100

    drop utils

--- graphed-graphql/src/wasmMain/kotlin/GraphQLToSexp.kt
+++
@@ -1,7 +0,0 @@
-import com.pcarrier.graphed.graphql.Parser
-import com.pcarrier.graphed.graphql.ToSexp
-
-fun graphqlToSexp(str: String): String {
-    val doc = Parser(str).parse()
-    return ToSexp.document(doc).toString()
-}
