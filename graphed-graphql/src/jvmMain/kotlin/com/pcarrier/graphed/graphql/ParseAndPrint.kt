ae4b3952af133e6af34e19ded61ccdf7c910df52
Pierre Carrier <pc@rrier.fr>
2023-05-29 22:07:49 +0100

    woops

--- graphed-graphql/src/jvmMain/kotlin/com/pcarrier/graphed/graphql/ParseAndPrint.kt
+++ graphed-graphql/src/jvmMain/kotlin/com/pcarrier/graphed/graphql/ParseAndPrint.kt
@@ -3,7 +3,7 @@ package com.pcarrier.graphed.graphql
 object ParseAndPrint {
     @JvmStatic
     fun main(args: Array<String>) {
-        val src = System.`in`.bufferedReader().readLine()
+        val src = String(System.`in`.readAllBytes())
         val doc = Parser(src).parse()
         val printed = Printer().printDocument(doc)
         println(printed)
